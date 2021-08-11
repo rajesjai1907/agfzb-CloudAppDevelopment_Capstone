9c7e5492e0f9b1004155f21187ca0b39350aeb31
Rajesh Jain <rajesjai@gmail.com>
2021-08-11 17:38:02 +0000

    commit for static website

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost",
+                "rajesjai-ng.eu-gb.mybluemix.net"]
 
 
 # Application definition
