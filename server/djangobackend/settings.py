ddc03ef3546950b37380891cd5b9f66249398e04
Rajesh Jain <rajesjai@gmail.com>
2021-08-16 10:29:53 +0000

    Changes to set ALLOWED_HOSTS

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,8 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost",
-                "rajesjai-ng.eu-gb.mybluemix.net"]
+ALLOWED_HOSTS = ["rajesjai-ng.eu-gb.mybluemix.net"]
 
 
 # Application definition
